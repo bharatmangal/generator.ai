cf003c959cc196aac12a63df81302ff784625dc2
bharatmangal <116515728+bharatmangal@users.noreply.github.com>
2024-12-12 12:49:37 +0530

    Delete utils directory

--- utils/extract_questions.py
+++
@@ -1,9 +0,0 @@
-from PyPDF2 import PdfReader
-
-def extract_questions(pdf_file):
-    reader = PdfReader(pdf_file)
-    questions = ""
-    for page in reader.pages:
-        questions += page.extract_text() + "\n"
-    return questions
-    print(questions)
